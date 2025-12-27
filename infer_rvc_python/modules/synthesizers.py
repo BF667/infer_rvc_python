95f617de30c552ff875d0fbb99e499a7faf68535
programer01 <festyus09@gmail.com>
2025-12-28 00:09:26 +0700

    Update synthesizers.py

--- infer_rvc_python/modules/synthesizers.py
+++ infer_rvc_python/modules/synthesizers.py
@@ -2,7 +2,7 @@ import os
 import sys
 import torch
 
-sys.path.append(os.getcwd())
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from modules.hifigan import HiFiGANGenerator
 from modules.refinegan import RefineGANGenerator
